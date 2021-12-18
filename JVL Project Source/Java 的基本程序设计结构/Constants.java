0f3f7d9026274da55643322c9155e478975c03c8
Sni-anr <anr15a08@gmail.com>
2021-12-18 16:51:05 +0800

    Sni

---
+++ JVL Project Source/Java 的基本程序设计结构/Constants.java
@@ -0,0 +1,11 @@
+public class Constants {
+    public static final double CM_PER_INCH = 2.54;
+
+    public static void main(String[] args) {
+        double paperWeight = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters:"
+                + paperWeight * CM_PER_INCH + " by "
+                + paperHeight * CM_PER_INCH);
+    }
+}
