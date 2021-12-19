fe83375d65408791b6d2fb6b2f8531dd8427cdbd
Sni-anr <anr15a08@gmail.com>
2021-12-19 15:14:18 +0800

    2021/12/13

--- JVL Project Source/Java 的基本程序设计结构/LengthCodePointCount.java
+++ JVL Project Source/Java 的基本程序设计结构/LengthCodePointCount.java
@@ -1,3 +1,4 @@
+// 测试 码元 码点 length codePointCount charAt offsetByCodePoints codePointAt
 public class LengthCodePointCount {
     public static String greeting = "Hello\uD835\uDD46"; // 𝕆
 
@@ -6,23 +7,30 @@ public class LengthCodePointCount {
         System.out.println(greeting + " code unit is " + unCode);
 
         for (int i = 0; i < 5; i++) {
-            System.out.print("==");
+            System.out.print("===");
         }
 
         int cp = greeting.codePointCount(0, greeting.length());
         System.out.println("\nThe code Point is " + cp);           //码点个数，一个 Unicode 字符就是一个码点。
 
         for (int i = 0; i < 5; i++) {
-            System.out.print("==");
+            System.out.print("===");
         }
 
         char first = greeting.charAt(0);
         char last = greeting.charAt(greeting.length() - 1);
 
-        System.out.println("\nThe first code unit is " + first );
-        System.out.println("The last code unit is " + last);
+        System.out.println("\nThe first code unit is " + first);   //第一个码元，显示字符 H
+        System.out.println("The last code unit is " + last);        //最后一个码元，辅助字符集𝕆 char 无法存储（？）
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("===");
+        }
+
+        int index = greeting.offsetByCodePoints(0, 5);
+        cp = greeting.codePointAt(index);
+        System.out.println("\nThe last code point is " + Integer.toHexString(cp));       //码点 一般为 16 进制数
     }
 
 }
 
-// 测试 码元 码点 length codePointCount charAt offsetByCodePoints codePointAt
---
+++ JVL Project Source/Java 的基本程序设计结构/inputTest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class inputTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your name : ");
+        String name = in.nextLine();
+
+        System.out.print("Enter your age : ");
+        int age = in.nextInt();
+
+        System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
+    }
+}
