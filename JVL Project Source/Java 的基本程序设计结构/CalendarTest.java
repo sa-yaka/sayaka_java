5cb2298b5535beb4c22008ea38e614e1d97b1322
Sni-anr <anr15a08@gmail.com>
2022-01-29 13:47:46 +0800

    CalendarTest

--- JVL Project Source/Java 的基本程序设计结构/CalendarTest.java
+++ JVL Project Source/Java 的基本程序设计结构/CalendarTest.java
@@ -1,58 +1,33 @@
-import java.text.DateFormatSymbols;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class CalendarTest {
     public static void main(String[] args) {
+        LocalDate date = LocalDate.now(); // 构造对象，初始化为当前日期
+        int month = date.getMonthValue(); // 获得当前的月份
+        int today = date.getDayOfMonth(); // 获得当前的日期
 
-        GregorianCalendar d = new GregorianCalendar();
+        date = date.minusDays(today - 1); // 将 date 设置为一周的第一天
+        DayOfWeek weekday = date.getDayOfWeek(); // 得到这一天为星期几
+        int value = weekday.getValue(); // 返回一个整数，表示当天为星期几 1 = Monday ... 6 = Saturday
 
-        int today = d.get(Calendar.DAY_OF_MONTH);
-        int month = d.get(Calendar.MONTH);
-
-        d.set(Calendar.DAY_OF_MONTH, 1);
-
-        int weekday = d.get(Calendar.DAY_OF_WEEK);
-
-        int firstDayOfWeek = d.getFirstDayOfWeek();
-
-        int indent = 0;
-        while (weekday != firstDayOfWeek) {
-            indent++;
-            d.add(Calendar.DAY_OF_MONTH, -1);
-            weekday = d.get(Calendar.DAY_OF_WEEK);
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (int i = 1; i < value; i++) { // 2022-01-29 时 value 为 6.
+            System.out.print("    ");
         }
 
-        String[] weekdayNames = new DateFormatSymbols().getShortWeekdays();
-        do {
-            System.out.printf("%4s", weekdayNames[weekday]);
-            d.add(Calendar.DAY_OF_MONTH, 1);
-            weekday = d.get(Calendar.DAY_OF_WEEK);
-        } while (weekday != firstDayOfWeek);
-        System.out.println();
-
-        for (int i = 1; i <= indent; i++)
-            System.out.print("    ");
-
-        d.set(Calendar.DAY_OF_MONTH, 1);
-        do {
-
-            int day = d.get(Calendar.DAY_OF_MONTH);
-            System.out.printf("%3d", day);
-
-            if (day == today)
-                System.out.print("*");
+        while (date.getMonthValue() == month) {
+            System.out.printf("%3d", date.getDayOfMonth());
+            if (date.getDayOfMonth() == today)
+                System.out.print("*"); // 是当天就打印 *
             else
                 System.out.print(" ");
-
-            d.add(Calendar.DAY_OF_MONTH, 1);
-            weekday = d.get(Calendar.DAY_OF_WEEK);
-
-            if (weekday == firstDayOfWeek)
+            date = date.plusDays(1); // 到下一天
+            if (date.getDayOfWeek().getValue() == 1) // 如果是一周的第一天就换行
                 System.out.println();
-        } while (d.get(Calendar.MONTH) == month);
+        }
 
-        if (weekday != firstDayOfWeek)
+        if (date.getDayOfWeek().getValue() != 1)
             System.out.println();
     }
 }
--- JVL Project Source/src/Welcome.java
+++ JVL Project Source/src/Welcome.java
@@ -1,9 +1,10 @@
+import java.time.LocalDate;
+
 public class Welcome {
     public static void main(String[] args) {
         String greeting = "Welcome to Java!";
         System.out.println(greeting);
         for (int i = 0; i < greeting.length(); i++)
             System.out.print("=");
-        System.out.println();
-    }
+        System.out.println();    }
 }
