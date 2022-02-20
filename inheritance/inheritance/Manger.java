d4dac26dfbf6142d51bb4d852089269617a34015
sa-yaka <anr15a08@gmail.com>
2022-02-20 11:12:18 +0800

    inheritance

---
+++ inheritance/inheritance/Manger.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+public class Manger extends Employee {
+
+    private double bonus;
+
+    public Manger(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double b) {
+        bonus = b;
+    }
+
+}
