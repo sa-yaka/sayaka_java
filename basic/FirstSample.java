b158485d5bbbe49f93abc1f7f078f40e7da967ac
sa-yaka <anr15a08@gmail.com>
2022-03-18 21:36:09 +0800

    更改

--- basic/FirstSample.java
+++
@@ -1,7 +0,0 @@
-public class FirstSample {
-    public static void main(String[] args) {
-        System.out.println("We will not use 'Hello,World!'");
-//        object.method(parameters)   println 每次调用都在新的一行上
-    }
-}
-
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        int array[] = { 1, 2, 3, 4 };
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-        }
+        System.out.println("Hello World");
     }
 }
