85de3a33363853c4fe7f2d427cbda09241ed9f5f
sa-yaka <anr15a08@gmail.com>
2022-03-26 19:20:47 +0800

    lambda

--- src/ship/Main.java
+++ src/ship/Main.java
@@ -4,29 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter the ship type: ");
-        String shipType = in.nextLine();
+        var kongou = new ship[4];
+        kongou[0] = new ship("BB", "KONGOU", "Kongou", 31720, 220);
+        kongou[1] = new ship("BB", "KONGOU", "Hiei", 36600, 222);
+        kongou[2] = new ship("BB", "KONGOU", "kirishima", 36668, 222);
+        kongou[3] = new ship("BB", "KONGOU", "kirishima", 32156, 222);
 
-        System.out.print("Enter the ship class: ");
-        String shipClassName = in.nextLine();
-
-        System.out.print("Enter the ship name: ");
-        String shipName = in.nextLine();
-
-        System.out.print("Enter the weight of the ship: ");
-        int allWeight = in.nextInt();
-
-        System.out.print("Enter the length of the ship: ");
-        int allLength = in.nextInt();
-
-        var kongou = new ship(shipType, shipClassName, shipName, allWeight, allLength);
-
-        System.out.println("\nThe ship name is " + kongou.getShipName());
-        System.out.println("The ship class is " + kongou.getShipClassName());
-        System.out.println("The ship type is " + kongou.getShipType());
-        System.out.println("The ship length is " + kongou.getAllLength());
-        System.out.println("The ship weight is " + kongou.getAllWeight());
+        for (ship e : kongou) {
+            System.out.println("\nThe ship name is " + e.getShipName());
+            System.out.println("The ship class is " + e.getShipClassName());
+            System.out.println("The ship type is " + e.getShipType());
+            System.out.println("The ship length is " + e.getAllLength());
+            System.out.println("The ship weight is " + e.getAllWeight());
+        }
     }
 }
