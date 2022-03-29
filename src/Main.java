b80919e34bd8492ac3acdb7ac6212dd60790f1ce
sa-yaka <anr15a08@gmail.com>
2022-03-29 21:48:12 +0800

    innerClass

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        char chrBell='\007';
+        System.out.println(chrBell);
+
     }
 }
