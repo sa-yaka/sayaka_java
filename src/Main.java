583a4b38e3f373f6ef77f619dea68a5c432b1627
sa-yaka <sx.sayaka@gmail.com>
2022-04-26 19:45:09 +0800

    file in out

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        char chrBell='\007';
-        System.out.println(chrBell);
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter("test.txt", StandardCharsets.UTF_8);
+        int i = 0;
+        while (i <= 100) {
+            out.println("Hello, World.\tHello, World.\tHello, World.\tHello, World.");
+            i++;
+        }
+        out.close();
+//
+//        Scanner in = new Scanner(Path.of("test.txt"), StandardCharsets.UTF_8);
+//        while (in.hasNext()) {
+//            String line = in.nextLine();
+//            System.out.println(line);
+//
+//        }
     }
 }
