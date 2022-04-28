5e3ebe66110adafdbcba3361e20c727fcd67c1bf
sa-yaka <sx.sayaka@gmail.com>
2022-04-28 21:12:33 +0800

    test

--- Old/InterfacesLambda/interfaces/EmployeeSortText.java
+++ Old/InterfacesLambda/interfaces/EmployeeSortText.java
@@ -8,10 +8,10 @@ public class EmployeeSortText {
 
         staff[0] = new Employee("Harry Hacker", 35000);
         staff[1] = new Employee("Carl Cracker", 75000);
-        staff[2] = new Employee("Tont Tester", 38000);
+        staff[2] = new Employee("Tony Tester", 38000);
 
         Arrays.sort(staff);
-        
+
         for (Employee e : staff) {
             System.out.println("name = " + e.getName() + "salary = " + e.getSalary());
         }
